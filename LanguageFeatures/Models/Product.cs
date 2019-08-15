e919eac62c7ab2f95037ccb6a0c7a1f525250b58
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-15 17:59:44 -0400

    Add automatic properties to Product.

--- LanguageFeatures/Models/Product.cs
+++ LanguageFeatures/Models/Product.cs
@@ -7,12 +7,12 @@ namespace LanguageFeatures.Models
 {
     public class Product
     {
-        private string name;
+        public int ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description{ get; set; }
 
-        public string Name
-        {
-            get { return name; }
-            set { name = value; }
-        }
+        public decimal Price { get; set; }
+        public string Category { get; set; }
+
     }
 }
