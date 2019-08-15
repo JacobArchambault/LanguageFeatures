3f59407f979bb23080c5308dff8e8cbc8d614799
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-15 17:50:42 -0400

    Add AutoProperty Viewresult to Controller

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using LanguageFeatures.Models;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Navigate to a URL to show an example";
+        }
+
+        public ViewResult AutoProperty()
+        {
+            // create a new Product oblect
+            Product myProduct = new Product();
+
+            // set the property value
+            myProduct.Name = "Kayak";
+
+            // get the property 
+            string productName = myProduct.Name;
+
+            // generate the view
+            return View("Result",
+                (object)string.Format("Product name: {0}", productName));
+        }
+    }
+}
