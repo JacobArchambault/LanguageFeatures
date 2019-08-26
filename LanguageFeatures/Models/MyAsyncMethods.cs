d5133bbeb7f2dc07b3aafc7581394438721f6773
Jacob Archambault <jacob.archambault@waystar.com>
2019-08-26 09:18:31 -0400

    Simplify MyAsyncMethods by using async and await keywords.

--- LanguageFeatures/Models/MyAsyncMethods.cs
+++ LanguageFeatures/Models/MyAsyncMethods.cs
@@ -9,18 +9,14 @@ namespace LanguageFeatures.Models
 {
     public class MyAsyncMethods
     {
-        public static Task<long?> GetPageLength()
+        public async static Task<long?> GetPageLength()
         {
             HttpClient client = new HttpClient();
 
-            var httpTask = client.GetAsync("http://apress.com");
+            var httpMessage = await client.GetAsync("http://apress.com");
 
             // we could do other things here while we are waiting for the HTTP request to complete.
-
-            return httpTask.ContinueWith((Task<HttpResponseMessage> antecedent) =>
-            {
-                return antecedent.Result.Content.Headers.ContentLength;
-            });
+            return httpMessage.Content.Headers.ContentLength;
         }
     }
 }
